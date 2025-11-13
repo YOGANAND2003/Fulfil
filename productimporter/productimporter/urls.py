a1067ca4428aa71718a12fb98f1cd22ea78fd914
YOGANAND2003 <jammisettyyoganand@gmail.com>
2025-11-14 03:22:53 +0530

    code added all working fine

--- productimporter/productimporter/urls.py
+++ productimporter/productimporter/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='product:upload_page', permanent=False), name='home'),
     path('admin/', admin.site.urls),
     path('product/', include('product.urls')),
 ]
